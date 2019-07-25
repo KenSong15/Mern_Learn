2ab77df1789efbbc8c5d7773edf943a733e463da
Ken <kensong96@gmail.com>
2019-07-25 14:35:43 -0400

    register with check work fine

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,7 +4,12 @@
 
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
 const { check, validationResult } = require("express-validator/check");
+
+//import the model
+const User = require("./../../models/User");
 
 //@route:   GET api/users
 //@desc:    Test route
@@ -27,15 +32,51 @@ router.post(
       "Please enter a password wit 6 or more characters"
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req); // catch all possible error
     if (!errors.isEmpty()) {
       // send back a bad response and show error array
       return res.status(400).json({ errors: errors.array() });
     }
-    //console.log(req.body);
 
-    res.send(req.body);
+    const { name, email, password } = req.body;
+
+    try {
+      // see if users exists
+      let user = await User.findOne({ email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists" }] }); //do this formatting to match the error message format
+      }
+
+      //get users gravatar
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm"
+      }); //setting up the avatar image property
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
+      //only making the user model but not actually saving it
+      //and the password is not encrypted yet
+
+      //encrypt password
+      const salt = await bcrypt.genSalt(10); //declear the level of encrypt
+      user.password = await bcrypt.hash(password, salt); //encrypt the password
+      await user.save(); //save the user to DB
+
+      //return jsonwebtoken
+
+      res.send("User registered");
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
   }
 );
 
